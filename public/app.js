f81b82ca01f3df0fabaf7c4a4c6dd9be876bbfbb
geanie blanco <geanieblanco@gmail.com>
2018-01-09 11:33:46 -0500

    homepage updates

--- public/app.js
+++ public/app.js
@@ -1,12 +1,36 @@
+// function lineOne(){
+//   var app = document.getElementById('line1');
+//   var typewriter = new Typewriter(app, {
+//     loop: false
+//   });
+//   typewriter.typeString(`Hi there! I'm Geanie. `)
+//     .pauseFor(1000)
+//     .start();
+// }
+//
+// function lineTwo(){
+//   var app = document.getElementById('line2');
+//   var typewriter = new Typewriter(app, {
+//     loop: false
+//   });
+//   typewriter.typeString(`How can I help you? `)
+//     .pauseFor(1000)
+//     .start();
+// };
+
 $(document).ready(function() {
-  //Homepage Typewriter
+
+  // lineOne();
+  // lineTwo();
+
+  // Homepage Typewriter
   var app = document.getElementById('title');
 
   var typewriter = new Typewriter(app, {
     loop: false
   });
 
-  typewriter.typeString(`Hi there! I'm Geanie... How can I help you? `)
+  typewriter.typeString("Hi there! I'm Geanie. \n How can I help you?")
     .pauseFor(1000)
     .start();
 
