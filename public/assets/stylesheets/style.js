0612cae7f59ca836e19474199bf5a5de7db53d71
geanie blanco <geanieblanco@gmail.com>
2018-03-09 13:02:30 -0500

    add web services

---
+++ public/assets/stylesheets/style.js
@@ -0,0 +1,89 @@
+{
+  styles: {
+    body: {
+      overflow: hidden !important
+    },
+    html: {
+      backgroundColor: #138997,
+      fontFamily: \ Catamaran\,
+      serif,
+      fontSize: 14 px
+    },
+    a: {
+      textDecoration: none,
+      decoration: none
+    },
+    a__hover: {
+      textDecoration: none
+    },
+    header: {
+      backgroundImage: url(\../images/header - background.jpg\),
+      backgroundSize: cover,
+      height: 450 px,
+      width: 100 %
+    },
+    title: {
+      marginTop: 210 px,
+      color: white,
+      fontSize: 60 px,
+      fontFamily: \ Give You Glory\,
+      \ Helvetica\,
+      cursive,
+      textAlign: center,
+      width: 50 %
+    },
+    button: {
+      backgroundColor: rgba(0, 0, 0, 0),
+      color: #F1FAEE,
+      borderWidth: 1 px,
+      borderColor: #F1FAEE,
+      borderRadius: 3 px
+    },
+    links: {
+      marginTop: 10 px,
+      backgroundColor: #138997,
+      textAlign: center
+    },
+    link_caption: {
+      marginTop: 10 px,
+      backgroundColor: #138997,
+      textAlign: center
+    },
+    about_icon: {
+      width: 30 %,
+      marginTop: 35 px
+    },
+    resume_icon: {
+      width: 30 %,
+      marginTop: 35 px
+    },
+    contact_icon: {
+      width: 30 %,
+      marginTop: 35 px
+    },
+    services_icon: {
+      width: 35 %,
+      marginTop: 35 px
+    },
+    info_caption: {
+      fontFamily: \ Give You Glory\,
+      \ Helvetica\,
+      cursive,
+      marginTop: 20 px,
+      color: black
+    },
+    info_caption_services: {
+      fontFamily: \ Give You Glory\,
+      \ Helvetica\,
+      cursive,
+      marginTop: 5 px,
+      color: black
+    },
+    footer: {
+      backgroundColor: #08777C
+    },
+    footer_h6: {
+      paddingTop: 5 px
+    }
+  }
+}
